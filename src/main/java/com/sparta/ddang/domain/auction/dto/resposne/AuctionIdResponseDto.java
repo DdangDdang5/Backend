6fa77b3f984fd3f48a671d35feb4761b6de6e5c6
pmjn1025 <pmjn1025@naver.com>
2022-09-07 19:25:30 +0900

    페이징 기능 구현 #38

---
+++ src/main/java/com/sparta/ddang/domain/auction/dto/resposne/AuctionIdResponseDto.java
@@ -0,0 +1,17 @@
+package com.sparta.ddang.domain.auction.dto.resposne;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class AuctionIdResponseDto {
+    private Long auctionId;
+
+    @Builder
+    public AuctionIdResponseDto(Long auctionId){
+
+        this.auctionId = auctionId;
+
+    }
+
+}
