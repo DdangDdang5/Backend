c55efc1d1aea14a39651ac9b8629c0cc7b544f16
shinyounseob1022 <83537005+shinyounseob1022@users.noreply.github.com>
2022-09-17 02:08:17 +0900

    feat: 마감임박 경매 4개 조회 기능 구현완료 #75

---
+++ src/main/java/com/sparta/ddang/domain/auction/dto/resposne/DeadlineAuctionResponseDto.java
@@ -0,0 +1,41 @@
+package com.sparta.ddang.domain.auction.dto.resposne;
+
+import com.sparta.ddang.domain.mulltiimg.entity.MultiImage;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class DeadlineAuctionResponseDto {
+
+    private String title;
+    private String content;
+    private Long nowPrice;
+    private List<MultiImage> multiImages;
+    private Long memberId;
+    private Long auctionId;
+    private boolean direct;
+    private boolean delivery;
+    private String region;
+    private LocalDateTime deadline;
+
+    @Builder
+    public DeadlineAuctionResponseDto(String title, String content, Long nowPrice,
+                                      List<MultiImage> multiImages, Long memberId, Long auctionId,
+                                      boolean direct, boolean delivery, String region, LocalDateTime deadline) {
+        this.title = title;
+        this.content = content;
+        this.nowPrice = nowPrice;
+        this.multiImages = multiImages;
+        this.memberId = memberId;
+        this.auctionId = auctionId;
+        this.direct = direct;
+        this.delivery = delivery;
+        this.region = region;
+        this.deadline = deadline;
+    }
+
+    public DeadlineAuctionResponseDto() {}
+}
--- src/main/java/com/sparta/ddang/domain/chat/repository/OnoChatMessageRepository.java
+++ src/main/java/com/sparta/ddang/domain/chat/repository/OnoChatMessageRepository.java
@@ -3,15 +3,11 @@ package com.sparta.ddang.domain.chat.repository;
 import com.sparta.ddang.domain.chat.entity.OnoChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OnoChatMessageRepository extends JpaRepository<OnoChatMessage,Long> {
+import java.util.List;
 
-    //boolean existsByRoomIdAndNickName(String roomId, String nickName);
+public interface OnoChatMessageRepository extends JpaRepository<OnoChatMessage,Long> {
 
     boolean existsByRoomId(String roomId);
 
-    //void deleteAllByRoomIdAndNickName(String roomId, String nickName);
-
-    void deleteAllByRoomId(String roomId);
-
-    //List<OnoChatMessage> findAllByNickName(String nickName);
+    List<OnoChatMessage> findAllByNickName(String nickName);
 }
