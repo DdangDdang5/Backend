37299f2748a4008991a99cff9cb4b25871e00984
shinyounseob1022 <83537005+shinyounseob1022@users.noreply.github.com>
2022-09-20 04:58:45 +0900

    fix: 경매 낙찰 후 1:1 채팅방을 무한 생성하던 오류 수정

--- src/main/java/com/sparta/ddang/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/sparta/ddang/domain/chat/repository/ChatRoomRepository.java
@@ -4,10 +4,13 @@ import com.sparta.ddang.domain.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom,Long> {
     List<ChatRoom> findAllByOrderByCreatedAtDesc();
 
     ChatRoom findByRoomId(String roomId);
 
+    List<ChatRoom> findAllByRoomName(String roomName);
+    Optional<ChatRoom> findByRoomName(String roomName);
 }
